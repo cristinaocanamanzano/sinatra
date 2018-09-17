6380124e8a209a8c1c589699487c48ad3b4a2f94
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-09-17 16:53:16 +0100

    Introducing parameters

--- app.rb
+++ app.rb
@@ -24,7 +24,13 @@ get '/lalala' do
   'lalala'
 end
 
-get '/cat' do
-  @random_name = ['Amigo', 'Oscar', 'Viking'].sample
+get '/random-cat' do
+  @name = ['Amigo', 'Oscar', 'Viking'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
